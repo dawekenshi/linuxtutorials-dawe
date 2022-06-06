c9b8deb4c392085557f1637689b5fda0158460c1
dawekenshi <104755414+dawekenshi@users.noreply.github.com>
2022-06-06 19:12:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function lightmode() {
+    document.querySelector('body').style.background = "white";
+    document.querySelector('.menu').style.background = "white";
+    document.querySelector('.spanish').style.background = "white";
+    document.querySelector('.english').style.background = "white";
+    document.querySelector('.light-mode').style.background = "white";
+    document.querySelector('.dark-mode').style.background = "white";
+}
+
+function darkmode() {
+    document.querySelector('body').style.background = "black";
+    document.querySelector('.menu').style.background = "black";
+    document.querySelector('.spanish').style.background = "black";
+    document.querySelector('.english').style.background = "black";
+    document.querySelector('.light-mode').style.background = "black";
+    document.querySelector('.dark-mode').style.background = "black";
+}
+
+function spanish() {
+    
+}
